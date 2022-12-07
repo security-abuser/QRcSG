ec8441699b6c746dd1cd00f18bcc8a9bf8ac0292
0x90 <0x90@0x90.0x90>
2022-12-07 19:45:31 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import PySimpleGUI as sg
+import cv2
+import pyqrcode
+
+"""
+    Demo of using OpenCV to show your webcam in a GUI window.
+    This demo will run on tkinter, Qt, and Web(Remi).  The web version flickers at the moment though
+    To exit, right click and choose exit. If on Qt, you'll have to kill the program as there are no right click menus
+    in PySimpleGUIQt (yet).
+"""
+
+sg.theme('Black')
+
+# define the window layout
+layout = [
+            [sg.Image(filename='', key='-IMAGE-', tooltip='Right click for exit menu')],
+            [sg.Button('Exit')],
+            [sg.InputText(key='-QR-'), sg.Button("GenQR")],
+
+]
+
+# create the window and show it without the plot
+window = sg.Window('Demo Application - OpenCV Integration', layout, location=(800,400),
+                   right_click_menu=['Right', ['Exit'], ], )
+
+# ---===--- Event LOOP Read and display frames, operate the GUI --- #
+cap = cv2.VideoCapture(1)                               # Setup the OpenCV capture device (webcam)
+detector = cv2.QRCodeDetector()
+while True:
+    event, values = window.read(timeout=20)
+    if event in ('Exit', None):
+        break
+
+    if event in ('GenQR'):
+        qr = pyqrcode.create(window['-QR-'])
+        sg.popup(image=qr.png())
+    ret, frame = cap.read()                             # Read image from capture device (camera)
+    imgbytes=cv2.imencode('.png', frame)[1].tobytes()   # Convert the image to PNG Bytes
+    window['-IMAGE-'].update(data=imgbytes)   # Change the Image Element to show the new image
+    data, points, _ = detector.detectAndDecode(frame)
+
+    if points is not None:
+        window['-QR-'].update(data)
+
+    else:
+        continue
+
+window.close()
