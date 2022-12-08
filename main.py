62bb141ad8c02e34d44154758f6be6a60f66bc77
0x90 <0x90@0x90.0x90>
2022-12-08 14:18:24 +0200

    Remove comments

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
 import PySimpleGUI as sg
 import cv2
 import pyqrcode
+import io
+from pyzbar import pyzbar
 
 
 sg.theme('Black')
 
 # define the window layout
 layout = [
-            [sg.Image(filename='', key='-IMAGE-', tooltip='Right click for exit menu')],
-            [sg.Button('Exit')],
+            [sg.Image(filename='', key='-IMAGE-')],
+            [sg.Button('Change camera')],
             [sg.InputText(key='-QR-'), sg.Button("GenQR")],
-
 ]
 
 # create the window and show it without the plot
-window = sg.Window('Demo Application - OpenCV Integration', layout, location=(800,400),
-                   right_click_menu=['Right', ['Exit'], ], )
+window = sg.Window('QRcSG', layout, location=(800, 400), )
+
+
+def pop_up(buf):
+    pop_up_layout = [[sg.Image(data=buf)], ]
+    sg.Window('QR', pop_up_layout).read()
+
 
 # ---===--- Event LOOP Read and display frames, operate the GUI --- #
-cap = cv2.VideoCapture(1)                               # Setup the OpenCV capture device (webcam)
-detector = cv2.QRCodeDetector()
+camera = 1
+cap = cv2.VideoCapture(camera)
+
 while True:
-    event, values = window.read(timeout=20)
-    if event in ('Exit', None):
+    try:
+        event, values = window.read(timeout=20)
+
+        if event == sg.WIN_CLOSED:
+            break
+
+        if event in 'Change camera':
+            camera = 1 if camera == 0 else 0
+            cap = cv2.VideoCapture(camera)
+            continue
+
+        if event in 'GenQR':
+            qr = pyqrcode.create(values['-QR-'], version=5, encoding='utf-8')
+            buffer = io.BytesIO()
+            qr.png(buffer, scale=10)
+            pop_up(buffer.getvalue())
+        ret, frame = cap.read()                             # Read image from capture device (camera)
+        imgbytes=cv2.imencode('.png', frame)[1].tobytes()   # Convert the image to PNG Bytes
+        window['-IMAGE-'].update(data=imgbytes)   # Change the Image Element to show the new image
+        qr_code = pyzbar.decode(frame)
+        if len(qr_code) != 0:
+            print(qr_code[0].data)
+            print(qr_code[0].data.decode('utf-8'))
+            window['-QR-'].update(qr_code[0].data.decode())
+        else:
+            continue
+    except:
+        sg.popup("Camera not found")
         break
-
-    if event in ('GenQR'):
-        qr = pyqrcode.create(window['-QR-'])
-        sg.popup(image=qr.png())
-    ret, frame = cap.read()                             # Read image from capture device (camera)
-    imgbytes=cv2.imencode('.png', frame)[1].tobytes()   # Convert the image to PNG Bytes
-    window['-IMAGE-'].update(data=imgbytes)   # Change the Image Element to show the new image
-    data, points, _ = detector.detectAndDecode(frame)
-
-    if points is not None:
-        window['-QR-'].update(data)
-
-    else:
-        continue
-
 window.close()
